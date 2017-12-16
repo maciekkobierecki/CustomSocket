8715f89fc97a409ed63bd82acd7d95cc0081cf9e
maciekkobierecki <maciekkobierecki@gmail.com>
2017-12-16 09:54:45 +0100

    Initial commit

---
+++ CustomSocket.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSocket
+{
+    class CustomSocket
+    {
+        Socket socket;
+        
+        public CustomSocket(IPAddress address, int port)
+        {
+             InitSocket(address, port);
+        }
+
+        private void InitSocket(IPAddress address, int port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddr = address;
+            IPEndPoint remoteEP = new IPEndPoint(ipAddr, port);
+            socket.Bind(remoteEP);
+        }
+
+        private Socket Accept()
+        {
+            return socket.Accept();
+        }
+
+        private Object ReceiveObject()
+        {
+            byte[] messageSize = new byte[4];
+            socket.Receive(messageSize, 0, 4, SocketFlags.None);
+            int inputSize = BitConverter.ToInt32(messageSize, 0);
+            byte[] bytes = new byte[inputSize];
+            int totalReceived = 0;
+            do
+            {
+                int received = socket.Receive(bytes, totalReceived, inputSize - totalReceived, SocketFlags.Partial);
+                totalReceived += received;
+            } while (totalReceived != inputSize);
+            Object ob = GetDeserializedObject(bytes);
+            return ob;
+        }
+
+
+        private Object GetDeserializedObject(byte[] b)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(b, 0, b.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Object deserialized = binForm.Deserialize(memStream);
+            return deserialized;
+        }
+
+        private void SendObject(Object toSendObject)
+        {
+            byte[] serializedObject = GetSerializedObject(toSendObject);
+            int messageSize = serializedObject.Length;
+            byte[] size = BitConverter.GetBytes(messageSize);
+            socket.Send(size);
+            socket.Send(serializedObject);
+
+        }
+
+        private byte[] GetSerializedObject(Object com)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, com);
+            return ms.ToArray();
+        }
+
+
+    }
+}
