f9e3d09407654cd4603d0c1e1a2a35f9bf6729fd
patrykgozdera <patryk.gozdera@gmail.com>
2017-12-20 16:16:04 +0100

    0.1.0
    logi logi logi

---
+++ LogClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSocket
+{
+    public class LogClass
+    {
+        public static void Log(string s)
+        {
+            Console.WriteLine("{0} | " + s, GetTimeStamp());
+        }
+
+        public static string GetTimeStamp()
+        {
+            DateTime dateTime = DateTime.Now;
+            return dateTime.ToString("HH:mm:ss.ff");
+        }
+    }
+}
