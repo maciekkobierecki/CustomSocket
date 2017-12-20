28b0a38ce272429fe52d4fc3b8aea869e19c339b
patrykgozdera <patryk.gozdera@gmail.com>
2017-12-20 15:02:18 +0100

    0.0.8
    klasa wiadomości

---
+++ MessageParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsst_client
+{
+    [Serializable]
+    public class MessageParameters
+    {
+        private string parameter_1;
+        private string parameter_2;
+        private int capacity;
+
+        public MessageParameters(string p1, string p2, int cap)
+        {
+            parameter_1 = p1;
+            parameter_2 = p2;
+            capacity = cap;
+        }
+
+        public String getFirstParameter()
+        {
+            return parameter_1;
+        }
+
+        public String getSecondParameter()
+        {
+            return parameter_2;
+        }
+
+        public int getCapacity()
+        {
+            return capacity;
+        }
+    }
+}
