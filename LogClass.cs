1689845ce7a709d5bc53479ba78f7c47493bf9bf
patrykgozdera <patryk.gozdera@gmail.com>
2018-01-16 22:47:27 +0100

    Kolorowe logsy

--- LogClass.cs
+++ LogClass.cs
@@ -13,6 +13,34 @@ namespace CustomSocket
             Console.WriteLine("{0} | " + s, GetTimeStamp());
         }
 
+        public static void WhiteLog(string s)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("{0} | " + s, GetTimeStamp());
+        }
+
+        public static void GreenLog(string s)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0} | " + s, GetTimeStamp());
+        }
+
+        public static void MagentaLog(string s)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("{0} | " + s, GetTimeStamp());
+        }
+
+        public static void CyanLog(string s)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{0} | " + s, GetTimeStamp());
+        }
+
         public static string GetTimeStamp()
         {
             DateTime dateTime = DateTime.Now;
